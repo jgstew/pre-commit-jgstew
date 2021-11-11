9fb136f22265e93ee325a0b64eca2cda246991da
jgstew <james@jgstew.com>
2021-11-11 14:15:45 -0500

    working pre-commit hooks, added precommit checks

--- pre_commit_hooks/minimum_changes.py
+++ pre_commit_hooks/minimum_changes.py
@@ -1,7 +1,9 @@
-"""Test pre-commit hook."""
+"""minimum changes required pre-commit hook."""
 
 import argparse
+import re
 import subprocess
+
 
 def build_argument_parser():
     """Build and return the argument parser."""
@@ -10,6 +12,11 @@ def build_argument_parser():
         description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
     )
     parser.add_argument("filenames", nargs="*", help="Filenames to check.")
+    parser.add_argument(
+        "--min-changes",
+        default="2",
+        help="Check for at least min-changes in git diff (defaults to 2).",
+    )
     return parser
 
 
@@ -22,9 +29,19 @@ def main(argv=None):
 
     retval = 0
     for filename in args.filenames:
-        print(filename)
+        output = subprocess.check_output(
+            ["git", "diff", "--numstat", "--cached", filename]
+        ).decode()
+        lines_add, lines_del = re.findall(r"(\d+)\s+(\d+)\s+", output)[0]
+
+        if max(int(lines_add), int(lines_del)) <= int(args.min_changes):
+            retval = retval + 1
+            print(
+                f"INFO: file `{filename}` does not have at least `{args.min_changes}` changes"
+            )
 
     return retval
+
 
 if __name__ == "__main__":
     exit(main())
