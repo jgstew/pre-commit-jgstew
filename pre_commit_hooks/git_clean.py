48ff0cac02acec0c167b7e8a22541ffae11a76a9
jgstew <james@jgstew.com>
2022-01-19 14:22:19 -0500

    new version, working autoclean

--- pre_commit_hooks/git_clean.py
+++ pre_commit_hooks/git_clean.py
@@ -1,11 +1,30 @@
 """git clean pre-commit hook."""
 
+import argparse
 import subprocess
 
 
-def git_clean():
+def build_argument_parser():
+    """Build and return the argument parser."""
+
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("filenames", nargs="*", help="Filenames to check.")
+    parser.add_argument(
+        "--auto-clean",
+        action="store_true",
+        help="DANGER! will delete untracked files automatically",
+    )
+    return parser
+
+
+def git_clean(auto_clean=False):
     """do git clean -f"""
-    run_cmd = ["git", "clean", "-f"]
+    cmd_flag = "-n"
+    if auto_clean:
+        cmd_flag = "-f"
+    run_cmd = ["git", "clean", cmd_flag]
     run_output = ""
     print(f"Running: {run_cmd}")
     try:
@@ -14,12 +33,15 @@ def git_clean():
         print(err)
 
     print(run_output)
-    return run_output.count("Removing ")
+    return run_output.count("Removing ") + run_output.count("Would remove ")
 
 
 def main(argv=None):
     """execution starts here"""
-    return git_clean()
+    # Parse command line arguments.
+    argparser = build_argument_parser()
+    args = argparser.parse_args(argv)
+    return git_clean(args.auto_clean)
 
 
 if __name__ == "__main__":
