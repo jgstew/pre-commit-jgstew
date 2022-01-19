f137558251f14e40df1ec6d6be7dd079b0f662da
jgstew <james@jgstew.com>
2022-01-19 13:41:50 -0500

    git clean pre-commit hook

--- pre_commit_hooks/git_clean.py
+++ pre_commit_hooks/git_clean.py
@@ -4,17 +4,23 @@ import subprocess
 
 
 def git_clean():
-    """do the git clean"""
+    """do git clean -f"""
     run_cmd = ["git", "clean", "-f"]
     run_output = ""
+    print(f"Running: {run_cmd}")
     try:
         run_output = subprocess.check_output(run_cmd).decode()
     except subprocess.CalledProcessError as err:
         print(err)
 
     print(run_output)
+    return run_output.count("Removing ")
 
 
 def main(argv=None):
     """execution starts here"""
-    git_clean()
+    return git_clean()
+
+
+if __name__ == "__main__":
+    exit(main())
